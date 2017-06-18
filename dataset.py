2396279be424e487a973cb92cccb6e4b8426e385
Mike Skaug <mike.skaug@gmail.com>
2017-06-17 21:59:57 -0600

    utilities for getting batches of training data

--- dataset.py
+++ dataset.py
@@ -1,5 +1,10 @@
 import os
+import math
+import random
+
 import pandas as pd
+import rasterio
+import numpy as np
 
 # Prepare the data for training and evaluating the model.
 
@@ -18,11 +23,81 @@ def unique_labels(labels):
     return list(unique_set)
 
 
-def get_labels(labels_file):
-    labels = pd.read_csv(labels_file)
+def get_labels(labels_file=LABEL_CSV):
+    names_and_labels = pd.read_csv(labels_file)
 
     # add a 'one-hot' vector of labels to each image
-    for label in unique_labels(labels):
-        labels[label] = labels['tags'].apply(lambda x: 1 if label in x.split(' ') else 0)
+    for label in unique_labels(names_and_labels):
+        names_and_labels[label] = names_and_labels['tags'].apply(lambda x: 1 if label in x.split(' ') else 0)
 
-    return labels
+    return names_and_labels
+
+
+def one_hot(tags_str, labels):
+    one_hot = [1 if label in tags_str.split(' ') else 0 for label in labels]
+    return one_hot
+
+
+def load_tiff(filename):
+    '''Return a 4D (r, g, b, nir) numpy array with the data in the specified TIFF filename.'''
+    with rasterio.open(filename) as src:
+        b, g, r, nir = src.read()
+        return np.dstack([r, g, b, nir])
+
+
+def get_data_sets(data_dir=TIF_DIR):
+    # The callable that will return sets of images to be used in training, validation and testing
+    #
+    # OUTPUT: a dictionary containing a Dataset object for each set [train, validation, test].
+
+    names_and_tags = pd.read_csv(LABEL_CSV)
+    labels = unique_labels(names_and_tags)
+    num_images = len(names_and_tags)
+    data_sets = {}
+    start_idx = 0
+
+    for set_name in SET_FRACTIONS.keys():
+        set_size = math.floor(num_images * SET_FRACTIONS[set_name])
+        image_set = []
+
+        for idx in range(start_idx, start_idx + set_size):
+            image_path = os.path.join(TIF_DIR, names_and_tags.iloc[idx]['image_name'] + '.tif')
+            label_vector = one_hot(names_and_tags.iloc[idx]['tags'], labels)
+            image_set.append((image_path, label_vector))
+
+        start_idx += set_size
+        random.shuffle(image_set)
+        data_sets[set_name] = Dataset(image_set)
+
+    return data_sets
+
+
+class Dataset:
+    '''
+    A class that stores image paths and labels and provides useful methods
+    for delivering batches of training data
+    '''
+
+    def __init__(self, data):
+        self.batch_idx = 0
+        self.data = data
+        self.num_examples = len(data)
+        (self.IMG_X, self.IMG_Y, self.IMG_D) = load_tiff(data[0][0]).shape
+        self.num_labels = len(data[0][1])
+
+    def get_image_batch(self, batch_size):
+        # return a new batch of images and labels
+        if self.batch_idx + batch_size > self.num_examples:
+            self.batch_idx = 0
+            random.shuffle(self.data)
+
+        image_array = np.empty(shape=(batch_size, self.IMG_X, self.IMG_Y, self.IMG_D), dtype=np.float32)
+        label_array = np.empty(shape=(batch_size, self.num_labels), dtype=np.int32)
+
+        for i, j in zip(range(self.batch_idx, self.batch_idx + batch_size), range(batch_size)):
+            # read the image file
+            image_array[j, :, :, :] = load_tiff(self.data[i][0])
+            label_array[j, :] = self.data[i][1]
+            self.batch_idx += 1
+
+        return (image_array, label_array)
